bf42a5f48232faedc715b8364767516ca9ca43c8
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-02-27 19:46:09 +0100

    change script.js style.css

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-const number = document.querySelectorAll('.number');
 const result = document.querySelector('.result');
-const signs = document.querySelectorAll('.sign');
-const clear = document.querySelector('.clear');
-equals = document.querySelector('.equals');
+const buttons = document.querySelectorAll('button');
 
-let firstValue = "";
-let isFirstValue = false;
-let secondValue = "";
-let isSecondValue = false;
-let sign = "";
-let resultValue = 0;
+let calculation = [];
+let accumulativeCalculation
 
-for(let i = 0; i <number.length; i++) {
-    number[i].addEventListener('click', (event) => {
-        let attribute = event.target.getAttribute('value');
-        if(isFirstValue === false) {
-            getFirstValue(attribute)
-        }
-        if(isSecondValue == false) {
-            getSecondValue(attribute);
-        }
-
-    })
-}
-
-function getFirstValue(element) {
-    result.innerHTML = "";
-    firstValue += element;
-    result.innerHTML = firstValue;
-    firstValue = +firstValue;
-}
-
-function getSecondValue(element) {
-    if(firstValue != "" && signs != "") {
-        secondValue += element;
-        result.innerHTML = secondValue;
-        secondValue = +secondValue;
+function calculate(button) {
+    const value = button.textContent;
+    if (value == "C") {
+        calculation = [];
+        result.textContent = '';
+    } else if (value === "=") {
+        result.textContent = eval(accumulativeCalculation);
+    } else {
+        calculation.push(value);
+        accumulativeCalculation =  calculation.join('');
+        result.textContent = accumulativeCalculation;
     }
-}
 
-function getSign() {
-    for(let i = 0; i < signs.length; i++) {
-        signs[i].addEventListener('click', (event) => {
-            signs = event.target.getAttribute('value');
-            isFirstValue = true;
-        })
-    }
-}
+    
+};
 
-getSign();
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
 
-equals.addEventListener('click', () => {
-    result.innerHTML = "";
-    if(sign === "/") {
-        resultValue = firstValue + secondValue;
-    } else if(sign === "-") {
-        resultValue = firstValue - secondValue;
-    } else if(sign === "x") {
 
-    }
-})
+
+
+
+// for(let i = 0; i <number.length; i++) {
+//     number[i].addEventListener('click', (e) => {
+//         let attribute = e.target.getAttribute('value');
+//         if(isFirstValue === false) {
+//             getFirstValue(attribute)
+//         }
+//         if(isSecondValue == false) {
+//             getSecondValue(attribute);
+//         }
+
+//     })
+// }
+
+// function getFirstValue(element) {
+//     result.innerHTML = "";
+//     firstValue += element;
+//     result.innerHTML = firstValue;
+//     firstValue = +firstValue;
+// }
+
+// function getSecondValue(element) {
+//     if(firstValue != "" && signs != "") {
+//         secondValue += element;
+//         result.innerHTML = secondValue;
+//         secondValue = +secondValue;
+//     }
+// }
+
+// function getSign() {
+//     for(let i = 0; i < signs.length; i++) {
+//         signs[i].addEventListener('click', (e) => {
+//             signs = e.target.getAttribute('value');
+//             isFirstValue = true;
+//         })
+//     }
+// }
+
+// getSign();
+
+// equals.addEventListener('click', () => {
+//     result.innerHTML = "";
+//     if(sign === "/") {
+//         resultValue = firstValue + secondValue;
+//     } else if(sign === "-") {
+//         resultValue = firstValue - secondValue;
+//     } else if(sign === "x") {
+
+//     }
+// })
